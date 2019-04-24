f62ac1fe649a02fab69d02baa79f30baff80665a
айкеновад <@205_8>
2019-04-24 10:35:59 +0600

    -

--- 24.04.2019_Lesson/Source.cpp
+++ 24.04.2019_Lesson/Source.cpp
@@ -1,1 +1,56 @@
-﻿
+﻿#include"Stack.h"
+
+int main() {
+	
+	Stack<double> s;
+	s.push(1.5);
+	s.push(2.2);
+	s.push(3.4);
+	s.push(9.5);
+	s.push(8.2);
+	s.push(7.7);
+	s.print();
+
+	s.pop();
+	cout << "last" << s.top() << endl;
+
+	while (!s.empty()) {//prints while stack s is not empty, the same: while(s.size()!=0)
+		cout << s.top() << endl;
+		s.pop();
+	}
+	if (s.empty()) cout << "empty now" << endl;
+
+	Stack<char> par;
+	string str;
+	int choice = 0;
+	while (true) {
+		cout << "Enter 1 to exit" << endl;
+		cout << "Enter 2 to continue" << endl;
+		cin >> choice;
+		if (choice == 1) break;
+		cout << "Enter a string" << endl;
+		cin >> str;
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (par.empty() && str[i] == ')') {
+				cout << "wrong" << endl;
+				break;
+			}
+			if (str[i] == '(') par.push('(');
+			if (str[i] == ')') par.pop();
+
+			if ((i == str.length()-1) && par.empty())
+				cout << "right" << endl;
+			else if ((i == str.length()-1) && !par.empty())
+				cout << "wrong" << endl;
+		}
+	}
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
--- 24.04.2019_Lesson/Stack.h
+++ 24.04.2019_Lesson/Stack.h
@@ -1,1 +1,108 @@
-﻿
+﻿#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<typename T>
+class Stack {
+private:
+	T* ptr;
+	int cur_size;
+	int buf_size;
+public:
+	Stack();
+	Stack(const Stack<T>& obj);
+	~Stack();
+	int size();
+
+	void push(T el);
+	void pop();
+	T &top();
+	bool empty()const;
+	void print();
+	Stack<T> operator=(const Stack<T>& obj); //когда конструктор можно не писать <T>, а когда метод то обязательно
+};
+
+template<typename T>
+inline Stack<T>::Stack()
+{
+	ptr = 0;
+	cur_size = 0;
+	buf_size = 0;
+}
+
+template<typename T>
+inline Stack<T>::Stack(const Stack<T>& obj)
+{
+	cur_size = obj.cur_size;
+	buf_size=obj.buf_size;
+	ptr = new T[buf_size];
+	for (int i = 0; i < cur_size; i++)
+		ptr[i] = obj.ptr[i];
+}
+
+template<typename T>
+inline Stack<T>::~Stack()
+{
+	delete[] ptr;
+}
+
+template<typename T>
+inline int Stack<T>::size()
+{
+	return cur_size;
+}
+
+template<typename T>
+inline void Stack<T>::push(T el)
+{
+	if (buf_size == 0) {
+		buf_size = 4;
+		ptr = new T[buf_size];
+	}
+	else if (cur_size == buf_size) {
+		buf_size *= 2;
+		T*tmp = new T[buf_size];
+		for (int i = 0; i < cur_size; i++)
+			tmp[i] = ptr[i];
+		delete[] ptr;
+		ptr = tmp;
+	}
+	ptr[cur_size++] = el;
+}
+
+template<typename T>
+inline void Stack<T>::pop()
+{
+	cur_size--;
+}
+
+template<typename T>
+inline T& Stack<T>::top()
+{
+	return ptr[cur_size-1];
+}
+
+template<typename T>
+inline bool Stack<T>::empty() const
+{
+	return (cur_size==0);
+}
+
+template<typename T>
+inline void Stack<T>::print()
+{
+	for (int i = 0; i < cur_size; i++)
+		cout << ptr[i] << endl;
+	cout << endl;
+}
+
+template<typename T>
+inline Stack<T> Stack<T>::operator=(const Stack<T>& obj)
+{
+	cur_size = obj.cur_size;
+	buf_size = obj.buf_size;
+	ptr = new T[buf_size];
+	for (int i = 0; i < cur_size; i++)
+		ptr[i] = obj.ptr[i];
+}
