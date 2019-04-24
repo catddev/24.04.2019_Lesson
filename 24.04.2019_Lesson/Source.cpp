47ed481006b4f41ae3c5681040e42f7dd6807769
айкеновад <@205_8>
2019-04-24 11:07:03 +0600

    -

--- 24.04.2019_Lesson/Source.cpp
+++ 24.04.2019_Lesson/Source.cpp
@@ -2,60 +2,61 @@
 
 int main() {
 	
-	//Stack<double> s;
-	//s.push(1.5);
-	//s.push(2.2);
-	//s.push(3.4);
-	//s.push(9.5);
-	//s.push(8.2);
-	//s.push(7.7);
-	//s.print();
+	Stack<double> s;
+	s.push(1.5);
+	s.push(2.2);
+	s.push(3.4);
+	s.push(9.5);
+	s.push(8.2);
+	s.push(7.7);
+	s.print();
 
-	//s.pop();
-	//cout << "last" << s.top() << endl;
+	s.pop();
+	cout << "last" << s.top() << endl;
 
-	//while (!s.empty()) {//prints while stack s is not empty, the same: while(s.size()!=0)
-	//	cout << s.top() << endl;
-	//	s.pop();
-	//}
-	//if (s.empty()) cout << "empty now" << endl;
+	while (!s.empty()) {//prints while stack s is not empty, the same: while(s.size()!=0)
+		cout << s.top() << endl;
+		s.pop();
+	}
+	if (s.empty()) cout << "empty now" << endl;
 
-	//Stack<char> par;
-	//string str;
-	//int choice = 0;
-	//while (true) {
-	//	cout << "Enter 1 to exit" << endl;
-	//	cout << "Enter 2 to continue" << endl;
-	//	cin >> choice;
-	//	if (choice == 1) break;
-	//	cout << "Enter a string" << endl;
-	//	cin >> str;
-	//	for (int i = 0; i < str.length(); i++)
-	//	{
-	//		if (par.empty() && str[i] == ')') {
-	//			cout << "wrong" << endl;
-	//			break;
-	//		}
-	//		if (str[i] == '(') par.push('(');
-	//		else if (str[i] == ')') par.pop();
+	Stack<char> par;
+	string str;
+	int choice = 0;
+	while (true) {
+		cout << "Enter 1 to exit" << endl;
+		cout << "Enter 2 to continue" << endl;
+		cin >> choice;
+		if (choice == 1) break;
+		cout << "Enter a string" << endl;
+		cin >> str;
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (par.empty() && str[i] == ')') {
+				cout << "wrong" << endl;
+				break;
+			}
+			if (str[i] == '(') par.push('(');
+			else if (str[i] == ')') par.pop();
 
-	//		if ((i == str.length()-1) && par.empty())
-	//			cout << "right" << endl;
-	//		else if ((i == str.length()-1) && !par.empty())
-	//			cout << "wrong" << endl;
-	//	}
-	//}
+			if ((i == str.length()-1) && par.empty())
+				cout << "right" << endl;
+			else if ((i == str.length()-1) && !par.empty())
+				cout << "wrong" << endl;
+		}
+	}
 
 	//проверить, чтобы в монетнице были только монеты меньше 10
 	cout << endl << endl;
 	Stack<int>money;
 	Stack<int>buf;
-	money.push(5);
-	money.push(1);
-	money.push(3);
-	money.push(10);
-	money.push(3);
-	money.print();
+	
+	int coin = -1;
+	while (coin != 0) {
+		cin >> coin;
+		money.push(coin);//закидываем все монеты
+	}
+	money.pop(); //erasing 0
 
 	while (!money.empty()) {
 		if (money.top() < 10) {
